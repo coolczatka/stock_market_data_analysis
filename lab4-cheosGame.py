1526ac5ad51b27e6c011eac21e242629fa434acf
LUDZKIESZCZENIE\LudzkieSzczenie <karolbaran0@gmail.com>
2021-05-29 12:35:45 +0200

    gra w chaos

---
+++ lab4-cheosGame.py
@@ -0,0 +1,42 @@
+import itertools
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation 
+import random
+
+def randomCorner(corners):
+    return random.choice(corners)
+
+def newPoint(point, corner):
+    return (
+        (point[0]+corner[0])/2,
+        (point[1]+corner[1])/2,
+    )
+
+def chaosGame(config):
+
+    points = [config['startPoint']]
+
+    for i in range(config['epochs']):
+        corner = randomCorner(config['corners'])
+        point = newPoint(points[-1], corner)
+        points.append(point)
+
+    x, y = zip(*points)
+    
+    plt.figure()
+    plt.scatter(x, y)
+    plt.show()
+
+config = {
+    'startPoint': (0, 0),
+    'corners': [(-1, -1), (1, -1), (0, 2)],
+    'epochs': 10
+}
+chaosGame(config)
+
+config['epochs'] = 100
+chaosGame(config)
+config['epochs'] = 1000
+chaosGame(config)
+config['epochs'] = 10000
+chaosGame(config)
